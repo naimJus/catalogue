6d60ae4d15285b353d90562feccc9b85cc396da2
Naim Jusufi <jusufi.naim@outlook.com>
2020-06-18 19:32:43 +0200

    architecture navigation rules

---
+++ app/src/main/java/com/example/catalogue/component/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.catalogue.component.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.catalogue.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
