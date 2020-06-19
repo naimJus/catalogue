e7d32e769fb551e76e908ad7551c6ffdae762dea
Naim Jusufi <jusufi.naim@outlook.com>
2020-06-19 17:27:03 +0200

    remove parcelable implementations

--- app/src/main/java/com/example/catalogue/data/beans/Business.kt
+++ app/src/main/java/com/example/catalogue/data/beans/Business.kt
@@ -5,71 +5,45 @@ import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class Business(
-    @SerializedName("id") val id: String?,
-    @SerializedName("alias") val alias: String?,
-    @SerializedName("name") val name: String?,
-    @SerializedName("image_url") val imageUrl: String?,
+    @SerializedName("id") val id: String,
+    @SerializedName("alias") val alias: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("image_url") val imageUrl: String,
     @SerializedName("is_closed") val isClosed: Boolean,
-    @SerializedName("url") val url: String?,
+    @SerializedName("url") val url: String,
     @SerializedName("review_count") val reviewCount: Int,
     @SerializedName("categories") val categories: List<Categories>,
     @SerializedName("rating") val rating: Double,
     @SerializedName("coordinates") val coordinates: Coordinates,
     @SerializedName("transactions") val transactions: List<String>,
-    @SerializedName("price") val price: String?,
+    @SerializedName("price") val price: String,
     @SerializedName("location") val location: Location,
-    @SerializedName("phone") val phone: String?,
-    @SerializedName("display_phone") val displayPhone: String?,
+    @SerializedName("phone") val phone: String,
+    @SerializedName("display_phone") val displayPhone: String,
     @SerializedName("distance") val distance: Double
-) : Parcelable {
+)
+
+data class BusinessRequiredData(val id: String, val imageUrl: String) : Parcelable {
     constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readByte() != 0.toByte(),
-        parcel.readString(),
-        parcel.readInt(),
-        parcel.createTypedArrayList(Categories)!!,
-        parcel.readDouble(),
-        parcel.readParcelable<Coordinates>(Coordinates::class.java.classLoader) as Coordinates,
-        parcel.createStringArrayList()!!,
-        parcel.readString(),
-        parcel.readParcelable<Location>(Location::class.java.classLoader) as Location,
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readDouble()
+        parcel.readString()!!,
+        parcel.readString()!!
     )
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeString(id)
-        parcel.writeString(alias)
-        parcel.writeString(name)
         parcel.writeString(imageUrl)
-        parcel.writeByte(if (isClosed) 1 else 0)
-        parcel.writeString(url)
-        parcel.writeInt(reviewCount)
-        parcel.writeTypedList(categories)
-        parcel.writeDouble(rating)
-        parcel.writeParcelable(coordinates, flags)
-        parcel.writeStringList(transactions)
-        parcel.writeString(price)
-        parcel.writeParcelable(location, flags)
-        parcel.writeString(phone)
-        parcel.writeString(displayPhone)
-        parcel.writeDouble(distance)
     }
 
     override fun describeContents(): Int {
         return 0
     }
 
-    companion object CREATOR : Parcelable.Creator<Business> {
-        override fun createFromParcel(parcel: Parcel): Business {
-            return Business(parcel)
+    companion object CREATOR : Parcelable.Creator<BusinessRequiredData> {
+        override fun createFromParcel(parcel: Parcel): BusinessRequiredData {
+            return BusinessRequiredData(parcel)
         }
 
-        override fun newArray(size: Int): Array<Business?> {
+        override fun newArray(size: Int): Array<BusinessRequiredData?> {
             return arrayOfNulls(size)
         }
     }
--- app/src/main/java/com/example/catalogue/data/beans/Location.kt
+++ app/src/main/java/com/example/catalogue/data/beans/Location.kt
@@ -14,40 +14,4 @@ data class Location(
     @SerializedName("country") val country: String?,
     @SerializedName("state") val state: String?,
     @SerializedName("display_address") val displayAddress: ArrayList<String>?
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readInt(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.createStringArrayList()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(address1)
-        parcel.writeString(address2)
-        parcel.writeString(address3)
-        parcel.writeString(city)
-        parcel.writeInt(zip_code)
-        parcel.writeString(country)
-        parcel.writeString(state)
-        parcel.writeStringList(displayAddress)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Location> {
-        override fun createFromParcel(parcel: Parcel): Location {
-            return Location(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Location?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+)
