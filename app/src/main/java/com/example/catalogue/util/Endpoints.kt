54b97d20bcf9b7dac844045123a7714786521389
Naim Jusufi <jusufi.naim@outlook.com>
2020-06-19 09:45:45 +0200

    fix endpoints

--- app/src/main/java/com/example/catalogue/util/Endpoints.kt
+++ app/src/main/java/com/example/catalogue/util/Endpoints.kt
@@ -2,7 +2,7 @@ package com.example.catalogue.util
 
 const val BASE_URL = "https://api.yelp.com/v3/"
 
-const val BUSINESS_SEARCH = "/businesses/search"
-const val BUSINESS_DETAILS = "/businesses/{id}/reviews"
-const val AUTOCOMPLETE = "/autocomplete"
-const val REVIEWS = "/businesses/{id}/reviews"
+const val BUSINESS_SEARCH = "businesses/search"
+const val BUSINESS_DETAILS = "businesses/{id}/reviews"
+const val AUTOCOMPLETE = "autocomplete"
+const val REVIEWS = "businesses/{id}/reviews"
